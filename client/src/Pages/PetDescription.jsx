4b0c0b538a9526ef8aa94af6be1acb432736ff97
Manu <belveacharya55@gmail.com>
2024-12-24 16:30:01 +0530

    pet adoption

--- client/src/Pages/PetDescription.jsx
+++ client/src/Pages/PetDescription.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, useParams } from "react-router-dom";
-import { InfinitySpin } from 'react-loader-spinner';
+import { InfinitySpin } from "react-loader-spinner";
 import { useAdoption } from "../context/PetContext";
 import axios from "axios";
-import {toast} from "react-toastify";
-
+import { toast } from "react-toastify";
 const PetDescription = () => {
   const { id } = useParams(); // Extract the pet ID from the route params
   const { user } = useAdoption(); // Get user context
@@ -39,10 +38,12 @@ const PetDescription = () => {
     fetchPetDetails();
   }, [id, user.token]);
 
+  console.log(pet);
   const handleAddToWishlist = async () => {
     try {
       const response = await axios.post(
-        `http://localhost:5000/api/v1/auth/${id}`,{},
+        `http://localhost:5000/api/v1/auth/${id}`,
+        {},
         {
           headers: {
             "Content-Type": "application/json",
@@ -52,7 +53,7 @@ const PetDescription = () => {
       );
       if (response.data.success) {
         toast.success("Pet added to wishlist successfully!");
-      }else{
+      } else {
         toast.error(response.data.message);
       }
     } catch (error) {
@@ -82,12 +83,15 @@ const PetDescription = () => {
     }
   };
 
-  if(loading) return ( <InfinitySpin
-    visible={true}
-    width="200"
-    color="#37B9F1"
-    ariaLabel="infinity-spin-loading"
-    />)
+  if (loading)
+    return (
+      <InfinitySpin
+        visible={true}
+        width="200"
+        color="#37B9F1"
+        ariaLabel="infinity-spin-loading"
+      />
+    );
 
   return (
     <>
@@ -96,50 +100,48 @@ const PetDescription = () => {
           <div className="banner">
             <h1>{pet.name}</h1>
           </div>
-          <div className="banner-image">
-            <img src={pet.imageUrl} alt={pet.name} className="pet-image" />
-          </div>
-          <div className="banner-details banner-column">
-            <div className="column">
-              <p>
-                <strong>Breed:</strong> {pet.breed}
-              </p>
-              <p>
-                <strong>Species:</strong> {pet.species}
-              </p>
-              <p>
-                <strong>Age:</strong> {pet.age} years
-              </p>
+
+          <div className="pet-details">
+            <img src={pet.imageUrl} alt="" />
+            <div className="pet-desc">
+              <div className="detail-row">
+                <h1 className="attribute">Breed : </h1>
+                <p className="value">{pet.breed}</p>
+              </div>
+              <div className="detail-row">
+                <h1 className="attribute">Species : </h1>
+                <p className="value">{pet.species}</p>
+              </div>
+              <div className="detail-row">
+                <h1 className="attribute">Age : </h1>
+                <p className="value">{pet.age}</p>
+              </div>
+              <div className="detail-row">
+                <h1 className="attribute">Description : </h1>
+                <p className="value">{pet.description}</p>
+              </div>
+              <div className="detail-row">
+                <h1 className="attribute">Shop Name : </h1>
+                <p className="value">{pet.shopId.name}</p>
+              </div>
+              <div className="detail-row">
+                <h1 className="attribute">Location : </h1>
+                <p className="value">{pet.shopId.location}</p>
+              </div>
+              <div className="detail-row">
+                <h1 className="attribute">Contact : </h1>
+                <p className="value">{pet.shopId.contactInfo}</p>
+              </div>
+
+              <div className="btn-col">
+                <button onClick={handleAddToWishlist} id="wishlist">
+                  Add to Wishlist
+                </button>
+                <button onClick={handleAdoptRequest} id="adopt">
+                  Adopt
+                </button>
+              </div>
             </div>
-            <p>{pet.description}</p>
-            <h1 className="pet-price">Price: ${pet.price}</h1>
-            <div className="btn-col">
-              <button onClick={handleAddToWishlist} id="wishlist">
-                Add to Wishlist
-              </button>
-              <button onClick={handleAdoptRequest} id="adopt">
-                Adopt
-              </button>
-            </div>
-          </div>
-          <div className="banner-details">
-            <img
-              src={pet.shopId.imageUrl}
-              alt={pet.shopId.name}
-              className="shop-logo"
-            />
-            <p>
-              <strong>Shop Name:</strong> {pet.shopId.name}
-            </p>
-            <p>
-              <strong>Location:</strong> {pet.shopId.location}
-            </p>
-            <p>
-              <strong>Contact:</strong>{" "}
-              <NavLink to={pet.shopId.contactInfo}>
-                {pet.shopId.contactInfo}
-              </NavLink>
-            </p>
           </div>
         </>
       )}
